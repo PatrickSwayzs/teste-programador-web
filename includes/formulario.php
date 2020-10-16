96e3b25fd70441a54cfdb1572af8cbe35a2d7767
Patrick Haas <patrick.hm@hotmail.com>
2020-10-16 13:18:45 -0300

    Versão inicial do formulário de cadastro

---
+++ includes/formulario.php
@@ -0,0 +1,66 @@
+<main>
+    <section>
+        <a href="index.php">
+            <button class="btn btn-success">Voltar</button>
+        </a>
+    </section>
+
+    <h2 class="mt-3">Cadastrar Venda</h2>
+
+    <form method="POST">
+        <div class="row">
+            <div class="form-group col-sm">
+                <label>Fornecedor</label>
+                <select class="form-control" name="fornecedores[]">
+                    <option selected="" disabled="">Selecione</option>
+                    <option></option>
+                </select>
+            </div>
+
+            <div class="form-group col-sm">
+                <label>Produto</label>
+                <select class="form-control" name="produtos">
+                    <option selected="" disabled="">Selecione Fornecedor Primeiro</option>
+                </select>
+            </div>
+
+            <div class="form-group col-sm">
+                <label>Preço</label>
+                <input type="text" class="form-control" name="preco">
+            </div>
+        </div>
+        <div class="row">
+            <div class="form-group col-sm">
+                <label>UF</label>
+                <input type="text" class="form-control" name="uf">
+            </div>
+
+            <div class="form-group col-sm">
+                <label>Cidade</label>
+                <input type="text" class="form-control" name="cidade">
+            </div>
+
+            <div class="form-group col-sm">
+                <label>Bairro</label>
+                <input type="text" class="form-control" name="bairro">
+            </div>
+        </div>
+        <div class="row">
+            <div class="form-group col-sm">
+                <label>Rua</label>
+                <input type="text" class="form-control" name="rua">
+            </div>
+
+            <div class="form-group col-sm">
+                <label>Data</label>
+                <input type="date" class="form-control" name="data">
+            </div>
+        </div>
+        <div class="form-group">
+            <button type="submit" class="btn btn-success">Enviar</button>
+        </div>
+
+
+    </form>
+
+</main>
---
+++ includes/listagem.php
@@ -0,0 +1,8 @@
+<main>
+    <section>
+        <a href="cadastrar.php">
+            <button class="btn btn-success">Nova Venda</button>
+        </a>
+    
+    </section>
+</main>
